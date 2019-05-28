d04f6082e392d2ccc52b8d9bbcd7dcfe11ac0df8
thoritie <thohirh9@gmail.com>
2019-05-28 21:36:04 +0700

    :zap: Change context config :snowman: :baby_bottle:

--- kubernetes-101/k8s/set-context.sh
+++ kubernetes-101/k8s/set-context.sh
@@ -1,6 +1,6 @@
-kubectl config set-cluster k8s-pronto-world-dev --server=https://108.128.179.69:6443
-kubectl config set clusters.k8s-pronto-world-dev.certificate-authority-data $(cat certificate/pronto_world_certificate_authority_data)
-kubectl config set-context kube-pronto-world --cluster=k8s-pronto-world-dev --user=kube-pronto-world-admin
-kubectl config set users.kube-pronto-world-admin.client-key-data $(cat certificate/pronto_world_client_key_data)
-kubectl config set users.kube-pronto-world-admin.client-certificate-data $(cat certificate/pronto_world_client_certificate_data)
-kubectl config use-context kube-pronto-world
+kubectl config set-cluster demo-k8s --server=https://18.191.198.18:6443
+kubectl config set clusters.demo-k8s.certificate-authority-data $(cat certificate/pronto_world_certificate_authority_data)
+kubectl config set-context kube-demo --cluster=demo-k8s --user=demo-admin
+kubectl config set users.demo-admin.client-key-data $(cat certificate/pronto_world_client_key_data)
+kubectl config set users.demo-admin.client-certificate-data $(cat certificate/pronto_world_client_certificate_data)
+kubectl config use-context kube-demo
